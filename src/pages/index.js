ce9d6c368aca28f16df7531d640cedaaca3268c3
nburton8 <nyshawn@cellhelmet.com>
2023-09-05 16:07:58 -0400

    buy me a coffee

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 
-export default function Home (props) {
+export default function Home() {
+  // Define inline styles for the support button
+  const supportButtonStyle = {
+    display: 'inline-block',
+    backgroundColor: '#FFDD00', // Customize the background color
+    color: '#000', // Customize the text color
+    padding: '10px 20px', // Add padding for spacing
+    borderRadius: '5px',
+    fontSize: '16px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    cursor: 'pointer',
+  };
+
   return (
     <>
       <h1>Rendering Modes Starter</h1>
@@ -13,6 +26,15 @@ export default function Home (props) {
           <Link to='/ssr'>SSR page</Link>
         </li>
       </ul>
+      <a
+        style={supportButtonStyle}
+        href="https://www.buymeacoffee.com/nyshawntru1"
+        target="_blank"
+        rel="noopener noreferrer" // Add this attribute
+      >
+        Buy Me a Coffee
+      </a>
+
     </>
   )
 }
